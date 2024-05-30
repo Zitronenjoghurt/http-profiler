165315e89ae27e8a9856a9ac1108f5f25ce0ebb3
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-05-30 21:13:07 +0200

    add latency profiling

--- src/profiler.rs
+++ src/profiler.rs
@@ -1,11 +1,28 @@
 use reqwest::{Client, Version};
+use tokio::time::{sleep, Duration};
 
-pub async fn profile_http(client: &Client, version: Version) -> u64 {
-    let res = client
-        .get("https://profiling.lemon.industries/")
+use crate::utils::timestamp_now_nanos;
+
+pub async fn profile_http(client: &Client, version: Version, iterations: u64) -> f32 {
+    let mut total_duration: u64 = 0;
+
+    for _ in 0..iterations {
+        let duration = ping_api(client, version).await;
+        total_duration += duration;
+        sleep(Duration::from_millis(500)).await;
+    }
+
+    (total_duration as f32) / (iterations as f32)
+}
+
+pub async fn ping_api(client: &Client, version: Version) -> u64 {
+    let before = timestamp_now_nanos();
+    client
+        .get("http://profiling.lemon.industries/")
         .version(version)
         .send()
         .await
         .unwrap();
-    0
+    let after = timestamp_now_nanos();
+    after - before
 }
