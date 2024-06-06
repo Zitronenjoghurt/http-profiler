b484f0bbadd4d9bc693d9e0170c64f8b6b735100
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-06-06 10:48:57 +0200

    just disable HTTP/3 + enable all crates of reqwest to check HTTP/2 compatibility on remote client

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,11 @@ async fn main() {
     let http_10 = profiler::profile_http(&client, Version::HTTP_10, 1).await;
     let http_11 = profiler::profile_http(&client, Version::HTTP_11, 1).await;
     let http_2 = profiler::profile_http(&client, Version::HTTP_2, 1).await;
-    let http_3 = profiler::profile_http(&client, Version::HTTP_3, 20).await;
+    //let http_3 = profiler::profile_http(&client, Version::HTTP_3, 20).await;
 
     //println!("HTTP/0.9: {}", http_09);
     println!("HTTP/1.0: {}", http_10);
     println!("HTTP/1.1: {}", http_11);
     println!("HTTP/2: {}", http_2);
-    println!("HTTP/3: {}", http_3);
+    //println!("HTTP/3: {}", http_3);
 }
