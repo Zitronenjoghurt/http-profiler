aa58a4eea5e08717b62cf59592c2bc50ae069dad
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-06-07 16:48:14 +0200

    remove unused quiche features

--- src/http3_client.rs
+++ src/http3_client.rs
@@ -1,29 +1,35 @@
-use quiche::{h3, Config};
-use std::{
-    net::{SocketAddr, UdpSocket},
-    str::FromStr,
-};
+use quiche::{h3, Config, Connection};
+use std::net::{ToSocketAddrs, UdpSocket};
 
 pub struct Http3Client {
-    conn: quiche::Connection,
+    conn: Connection,
     h3_conn: h3::Connection,
 }
 
 impl Default for Http3Client {
     fn default() -> Self {
-        let socket = UdpSocket::bind("0.0.0.0:0").expect("Failed to bind socket");
+        let socket = UdpSocket::bind("[::]:0").expect("Failed to bind socket");
         let mut config = Config::new(quiche::PROTOCOL_VERSION).expect("Failed to create config");
-        config.set_application_protos(&[b"example-proto"]).unwrap();
+        config
+            .set_application_protos(quiche::h3::APPLICATION_PROTOCOL)
+            .unwrap();
         config.verify_peer(false);
 
         let scid = quiche::ConnectionId::from_ref(&[0xba; 16]);
 
         let local_addr = socket.local_addr().expect("Failed to get local address");
-        let peer_addr =
-            SocketAddr::from_str("profiling.lemon.industries:443").expect("Invalid peer address");
+
+        let peer_addr = "profiling.lemon.industries:443"
+            .to_socket_addrs()
+            .expect("Failed to resolve peer address")
+            .next()
+            .expect("No addresses found for peer");
+
+        println!("Connecting to {}", peer_addr);
 
         let mut conn = quiche::connect(None, &scid, local_addr, peer_addr, &mut config)
             .expect("Failed to create connection");
+
         let h3_conn = h3::Connection::with_transport(
             &mut conn,
             &h3::Config::new().expect("Failed to create h3 config"),
